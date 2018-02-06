b521d2dae36784f6ce4baf05e2af3a8e8016621a
Burak İnce <burak.ince@linux.org.tr>
2018-02-06 18:51:51 +0300

    Add documantation for Cake

--- src/Cake.DependencyCheck/DependencyCheckAliases.cs
+++ src/Cake.DependencyCheck/DependencyCheckAliases.cs
@@ -4,9 +4,59 @@ using Cake.Core.Annotations;
 
 namespace Cake.DependencyCheck
 {
+    /// <summary>
+    /// <para>
+    /// Contains functionality for running a OWASP Dependency Check analysis on a C# project.
+    /// </para>
+    /// <para>
+    /// In order to use the commands for this addin, include the following in your build.cake file to download and
+    /// reference from NuGet.org:
+    /// <code>
+    ///     #tool "nuget:?package=DependencyCheck.Runner.Tool&include=./**/dependency-check.sh&include=./**/dependency-check.bat"
+    ///     #addin "nuget:?package=Cake.DependencyCheck"
+    /// </code>
+    /// </para>
+    /// </summary>
+    /// <example>
+    /// Project and Scan attributes required.
+    /// Also Format can be (XML, HTML, CSV, JSON, VULN, ALL). The default is HTML.
+    /// 
+    /// <code>
+    /// #tool "nuget:?package=DependencyCheck.Runner.Tool&include=./**/dependency-check.sh&include=./**/dependency-check.bat"
+    /// #addin "nuget:?package=Cake.DependencyCheck"
+    /// 
+    /// Task("Dependency-Check")
+    ///    .Does(() => {
+    ///       DependencyCheck(new DependencyCheckSettings
+    ///       {
+    ///          Project = "ExampleProjectName",
+    ///          Scan = "source/directory/*",
+    ///          Format = "HTML"
+    ///       });
+    ///   });
+    /// </code>
+    /// </example>
     [CakeAliasCategory("DependencyCheck")]
     public static class DependencyCheckAliases
     {
+        /// <summary>
+        /// Run the actual OWASP Dependency Check analysis.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Task("Dependency-Check")
+        ///    .Does(() => {
+        ///       DependencyCheck(new DependencyCheckSettings
+        ///       {
+        ///          Project = "ExampleProjectName",
+        ///          Scan = "source/directory/*",
+        ///          Format = "HTML"
+        ///       });
+        ///   });
+        /// </code>
+        /// </example>
+        /// <param name="context"></param>
+        /// <param name="settings">A required settings object.</param>
         [CakeMethodAlias]
         public static void DependencyCheck(this ICakeContext context, DependencyCheckSettings settings)
         {
--- src/Cake.DependencyCheck/DependencyCheckSettings.cs
+++ src/Cake.DependencyCheck/DependencyCheckSettings.cs
@@ -10,53 +10,106 @@ using Cake.Core.Tooling;
 
 namespace Cake.DependencyCheck
 {
+    /// <summary>
+    /// Required:
+    ///  - Project
+    ///  - Scan
+    /// </summary>
     public class DependencyCheckSettings : ToolSettings
     {
+        /// <summary>
+        /// The name of the project being scanned.
+        /// </summary>
         [Argument("--project")]
         public string Project { get; set; }
 
+        /// <summary>
+        /// The path to scan - this option can be specified multiple times. It is also possible to specify Ant style paths (e.g. directory/*/.jar).
+        /// </summary>
         [Argument("--scan")]
         public string Scan { get; set; }
 
+        /// <summary>
+        /// The path patterns to exclude from the scan - this option can be specified multiple times. This accepts Ant style path patterns (e.g. /exclude/).
+        /// </summary>
         [Argument("--exclude")]
         public string Exclude { get; set; }
 
+        /// <summary>
+        /// The depth that symbolic links will be followed; the default is 0 meaning symbolic links will not be followed.
+        /// </summary>
         [Argument("--symLink")]
         public string SymLink { get; set; }
 
+        /// <summary>
+        /// The folder to write reports to. This defaults to the current directory. If the format is not set to ALL one could specify a specific file name.
+        /// </summary>
         [Argument("--out")]
         public string Out { get; set; }
 
+        /// <summary>
+        /// The output format to write to (XML, HTML, CSV, JSON, VULN, ALL). The default is HTML.
+        /// </summary>
         [Argument("--format")]
         public string Format { get; set; }
 
+        /// <summary>
+        /// If the score set between 0 and 10 the exit code from dependency-check will indicate if a vulnerability with a CVSS score equal to or higher was identified.
+        /// </summary>
         [Argument("--failOnCVSS")]
         public string FailOnCVSS { get; set; }
 
+        /// <summary>
+        /// The file path to write verbose logging information.
+        /// </summary>
         [Argument("--log")]
         public string Log { get; set; }
 
+        /// <summary>
+        /// Disables the automatic updating of the CPE data.
+        /// </summary>
         [Flag("--noupdate")]
         public bool Noupdate { get; set; }
 
+        /// <summary>
+        /// The file paths to the suppression XML files; used to suppress false positives. This can be specified more than once to utilize multiple suppression files.
+        /// </summary>
         [Argument("--suppression")]
         public string Suppression { get; set; }
 
+        /// <summary>
+        /// Print the help message.
+        /// </summary>
         [Flag("--help")]
         public bool Help { get; set; }
 
+        /// <summary>
+        /// Print the advanced help message.
+        /// </summary>
         [Flag("--advancedHelp")]
         public bool AdvancedHelp { get; set; }
 
+        /// <summary>
+        /// Print the runner version information.
+        /// </summary>
         [Flag("--version")]
         public bool Version { get; set; }
 
+        /// <summary>
+        /// The number of hours to wait before checking for new updates from the NVD. The default is 4 hours.
+        /// </summary>
         [Argument("--cveValidForHours")]
         public string CveValidForHours { get; set; }
 
+        /// <summary>
+        /// Enable the experimental analyzers. If not set the analyzers marked as experimental below will not be loaded or used.
+        /// </summary>
         [Flag("--enableExperimental")]
         public bool EnableExperimental { get; set; }
 
+        /// <summary>
+        /// Enable the retired analyzers. If not set the analyzers marked as retired below will not be loaded or used.
+        /// </summary>
         [Flag("--enableRetired")]
         public bool EnableRetired { get; set; }
 
